0d0d9b381d71a9c19c697f023972568db889b19e
kelvin david <kelvndavd86@gmail.com>
2023-04-10 20:06:47 +0300

    toasted added successfully

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import  {toast}  from "react-toastify";
 const Form = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -6,6 +7,15 @@ const Form = () => {
   const [subject, setSubject] = useState('');
   const [error, setError] = useState();
 
+  const showToast = (error)=> {
+    if(error){
+      toast('Invalid data entered, try again', { hideProgressBar: false, autoClose: 2000, type: 'error' });
+    }
+    else{
+      toast("Email sent successfully", {hideProgressBar: false, autoClose: 2000, type: 'success'})
+    }
+    
+  }
   const handleSubmit = async (e)=> {
     e.preventDefault();
 
@@ -23,8 +33,10 @@ const Form = () => {
       setEmail('')
       setSubject('');
       setMessage('')
+      showToast(false);
     }
     else{
+      showToast(true);
       setError(true);
      
     }
@@ -53,7 +65,7 @@ const Form = () => {
             <path d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z" fill="currentColor" />
           </svg>
         </button>
-        {error? <span className="bg-red-400 text-lg my-5 p-3">Invalid data entered</span>:<span className="bg-green-400 text-lg my-5 p-3">Email Sent successful</span>}
+       
       </div>
     </form>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <> 
            
            <Component {...pageProps} />
+           <ToastContainer />
         </>
   
 }
